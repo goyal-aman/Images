3492e4fd608143877a9559aea7637bce5ec83a79
goyal-aman <goyal.amanrocks@gmail.com>
2020-12-27 01:35:05 +0530

    bug fix: css not loading wrong mime type

--- timeline/settings.py
+++ timeline/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True) # https://stackoverflow.com/questions/35557129/css-not-loading-wrong-mime-type-django
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
